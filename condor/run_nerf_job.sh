6e1c3119e5039f6d3bfe5447bf26b237cecb93da
George Kouros <george.kouros.ece@gmail.com>
2022-04-04 14:53:21 +0200

    Add dir creation

--- condor/run_nerf_job.sh
+++ condor/run_nerf_job.sh
@@ -2,6 +2,7 @@
 echo 'BaRF job started'
 EXP=$1
 NAME=$2
+
 source /users/visics/gkouros/.bashrc
 export PATH="/usr/local/cuda-11/bin:/usr/local/cuda/bin:$PATH"
 export LD_LIBRARY_PATH="/usr/local/cuda-11/lib64:/usr/local/cuda/lib64:$LD_LIBRARY_PATH"
@@ -9,6 +10,7 @@ conda activate barf-env
 dir=/users/visics/gkouros/projects/nerf-repos/bundle-adjusting-NeRF/
 cd $dir
 
+mkdir -p $2/$1
 python3 train.py --group=$2 --model=barf --yaml=barf_iphone --name=$1 --data.scene=chrysler --barf_c2f=[0.1,0.5]
 python3 evaluate.py --group=$2 --model=barf --yaml=barf_iphone --name=$1 --data.scene=chrysler --resume
 
