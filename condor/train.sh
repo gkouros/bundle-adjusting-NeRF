3608dd813ca925241811d1174b3534e746ad171c
George Kouros <george.kouros.ece@gmail.com>
2022-09-16 16:28:38 +0200

    Update condor files

---
+++ condor/train.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+NAME=$1
+EXP=$2
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate barf-env
+
+export PATH="/usr/local/cuda-11/bin:/usr/local/cuda/bin:$PATH"
+export LD_LIBRARY_PATH="/usr/local/cuda-11/lib64:/usr/local/cuda/lib64:$LD_LIBRARY_PATH"
+
+conda activate barf-env
+
+dir=/users/visics/gkouros/projects/nerf-repos/bundle-adjusting-NeRF/
+cd $dir
+mkdir -p output/$NAME/$EXP
+python3 evaluate.py \
+  --group=$NAME \
+  --model=barf \
+  --yaml=barf_llff \
+  --name=$EXP \
+  --data.scene=$NAME
+
+
+conda deactivate
